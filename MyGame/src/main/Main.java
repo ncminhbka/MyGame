296fc264d1f4c9b3beec5ade7913dcbabc4f7481
vinhdangquoc729 <158055080+vinhdangquoc729@users.noreply.github.com>
2024-09-26 23:42:25 +0700

    Update Main.java

--- MyGame/src/main/Main.java
+++ MyGame/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import javax.swing.JFrame;
 
 public class Main {
+	
 	public static void main(String[] args) {
         JFrame window = new JFrame(); // Tạo JFrame để quản lý cửa sổ game
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Thoát khi nhấn nút đóng cửa sổ
